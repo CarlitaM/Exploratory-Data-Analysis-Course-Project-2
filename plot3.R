c5e3b8109a7676a147403d2e2231d2d87e2c7098
CarlitaM <carlamusitrev@gmail.com>
2016-05-16 11:20:30 +0200

    plot3.R

--- plot3.R
+++ plot3.R
@@ -1,1 +1,12 @@
+## Download dataset from : https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip
 
+## Set your working directory with destination folder
+
+setwd("/Users/carlamusi/Desktop/Data Science/4. Exploratory Data Analysis/Week 4/02 Course Project 2/exdata-data-NEI_data")
+
+## Read the two files into R
+
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+## Question 3 : 
