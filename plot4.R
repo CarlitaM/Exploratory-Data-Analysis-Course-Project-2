303f3f11d0fb0ef1c16ad67e31bae73a902e79de
CarlitaM <carlamusitrev@gmail.com>
2016-05-16 17:51:39 +0200

    plot4.R

--- plot4.R
+++ plot4.R
@@ -2,7 +2,7 @@
 
 ## Set your working directory with destination folder
 
-setwd("/Users/carlamusi/Desktop/Data Science/4. Exploratory Data Analysis/Week 4/02 Course Project 2/exdata-data-NEI_data")
+setwd("/Users/Desktop/Data Science/4. Exploratory Data Analysis/Week 4/02 Course Project 2/exdata-data-NEI_data")
 
 ## Read the two files into R
 
